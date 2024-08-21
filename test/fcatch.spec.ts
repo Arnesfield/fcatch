9cafc85434194c1d2e052a20681a5e4ac96df182
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-08-21 10:30:03 +0800

    Update spec

--- test/fcatch.spec.ts
+++ test/fcatch.spec.ts
@@ -1,11 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { expect } from 'chai';
 import { fcatch, Result } from '../src/index.js';
 
-function expectResultData<T, E>(
-  result: Result<T, E>,
-  ok: boolean,
-  value?: T | E
-) {
+function expectResult<T, E>(result: Result<T, E>, ok: boolean, value?: T | E) {
   expect(result).to.be.an('array').that.has.length(2);
   expect(result).to.have.property('ok').that.is.a('boolean').that.equals(ok);
   expect(result).to.have.property('value');
@@ -45,43 +42,58 @@ describe('fcatch', () => {
     fcatch<Error>(error => error as Error);
   });
 
-  it('sync and async should return another function', () => {
+  it('sync call should return a function', () => {
     expect(fcatch().sync(() => {})).to.be.a('function');
+  });
+
+  it('async call should return a function', () => {
     expect(fcatch().async(() => {})).to.be.a('function');
   });
 
-  function testResultObject(result: Result<number, unknown>) {
-    expectResultData(result, true, 3);
+  function testResultObject(result: Result<number>) {
+    expectResult(result, true, 3);
   }
 
   function testCatchErrors(result: Result<number, Error>, error: Error) {
-    expectResultData(result, false, error);
+    expectResult(result, false, error);
   }
 
   function testDiscriminatedUnions(result: Result<number, Error>) {
-    // type checks
     if (result.ok) {
       expect(result.value.toString()).to.equal('3');
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      expect(result[1].toString()).to.equal('3');
       // @ts-expect-error
       expect(result.error?.message).to.be.undefined;
+      // @ts-expect-error
+      expect(result[0]?.message).to.be.undefined;
     } else {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-expect-error
       expect(result.value?.toString()).to.be.undefined;
+      // @ts-expect-error
+      expect(result[1]?.toString()).to.be.undefined;
       expect(result.error.message).to.be.a('string');
+      expect(result[0].message).to.be.a('string');
     }
+
     const [error, value] = result;
     if (error) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      expect(result.ok === true).to.be.false;
       // @ts-expect-error
       expect(value?.toString()).to.be.undefined;
+      // @ts-expect-error
+      expect(result.value?.toString()).to.be.undefined;
       expect(error.message).to.be.a('string');
+      expect(result.error.message).to.be.a('string');
     } else {
+      // @ts-expect-error
+      expect(result.ok === false).to.be.false;
       expect(value.toString()).to.equal('3');
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      expect(result.value.toString()).to.equal('3');
       // @ts-expect-error
       expect(error?.message).to.be.undefined;
+      // @ts-expect-error
+      expect(result.error?.message).to.be.undefined;
     }
   }
 
@@ -89,7 +101,7 @@ describe('fcatch', () => {
     result: Result<number, string>,
     message: string
   ) {
-    expectResultData(result, false);
+    expectResult(result, false);
     expect(result.error).to.be.a('string').that.equals(message);
     expect(result[0]).to.be.a('string').that.equals(message);
   }
@@ -105,6 +117,17 @@ describe('fcatch', () => {
       });
     });
   }
+
+  interface ThisArg {
+    value: string;
+    greet(this: this, greet: string): string;
+  }
+  const thisArg: ThisArg = {
+    value: 'World',
+    greet(greet: string) {
+      return `${greet} ${this.value}!`;
+    }
+  };
 
   describe('sync', () => {
     it('should return a Result object', () => {
@@ -144,6 +167,30 @@ describe('fcatch', () => {
         return n;
       })(1);
       testMapCaughtErrors(result, message);
+    });
+
+    it('should handle different return types', () => {
+      const fc1: Result<number> = fcatch().sync(() => 1)();
+      expect(fc1.value).to.equal(1);
+      const fc2: Result<string, Error> = fcatch<Error>().sync(() => '1')();
+      expect(fc2.value).to.equal('1');
+      const fc3: Result<Result<string, Error>> = fcatch().sync(() => fc2)();
+      expect(fc3.value).to.equal(fc2);
+    });
+
+    it('should handle `this` keyword', () => {
+      const greet = fcatch().sync(thisArg.greet);
+      // @ts-expect-error
+      const errResult = greet('Hello');
+      expectResult(errResult, false);
+      expect(errResult.error).to.be.an.instanceOf(TypeError);
+      const result = greet.call(thisArg, 'Hello');
+      expectResult(result, true, 'Hello World!');
+      const result2 = greet.call(
+        { value: 'FCatch', greet: thisArg.greet },
+        'Hi'
+      );
+      expectResult(result2, true, 'Hi FCatch!');
     });
   });
 
@@ -193,27 +240,66 @@ describe('fcatch', () => {
       })(1);
       testMapCaughtErrors(result, message);
     });
+
+    it('should handle different return types', async () => {
+      const fc1: Result<number> = await fcatch().async(() => 1)();
+      expect(fc1.value).to.equal(1);
+      const fc2: Result<string, Error> = await fcatch<Error>().async(
+        async () => {
+          await delay();
+          return '1';
+        }
+      )();
+      expect(fc2.value).to.equal('1');
+      const fc3: Result<Result<string, Error>> = await fcatch().async(
+        async () => fc2
+      )();
+      expect(fc3.value).to.equal(fc2);
+    });
+
+    it('should handle `this` keyword', async () => {
+      const greet = fcatch().async(thisArg.greet);
+      // @ts-expect-error
+      const errResult = await greet('Hello');
+      expectResult(errResult, false);
+      expect(errResult.error).to.be.an.instanceOf(TypeError);
+      const result = await greet.call(thisArg, 'Hello');
+      expectResult(result, true, 'Hello World!');
+      const result2 = await greet.call(
+        { value: 'FCatch', greet: thisArg.greet },
+        'Hi'
+      );
+      expectResult(result2, true, 'Hi FCatch!');
+    });
   });
 
   describe('resolve', () => {
-    it('should resolve promise (resolved)', async () => {
+    it('should handle resolved promise', async () => {
       const promise = fcatch().resolve(delay());
       expect(promise).to.be.a('promise');
       const result = await promise;
-      expectResultData(result, true, 0);
+      expectResult(result, true, 0);
     });
 
-    it('should resolve promise (rejected)', async () => {
-      const promise = fcatch(error => {
-        return error instanceof Error ? error : new Error('error');
-      }).resolve(delay(1));
+    it('should handle rejected promise', async () => {
+      const promise = fcatch<Error>().resolve(delay(1));
       expect(promise).to.be.a('promise');
       const result = await promise;
-      expectResultData(result, false);
+      expectResult(result, false);
       expect(result.error)
         .to.be.an('error')
         .that.has.a.property('message')
         .that.equals('Not implemented.');
+    });
+
+    it('should map caught errors', async () => {
+      const promise = fcatch(error => {
+        return error instanceof Error ? error.message : 'error';
+      }).resolve(delay(1));
+      expect(promise).to.be.a('promise');
+      const result = await promise;
+      expectResult(result, false);
+      expect(result.error).to.be.a('string').that.equals('Not implemented.');
     });
   });
 });
