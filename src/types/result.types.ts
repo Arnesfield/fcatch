1c579f8cb0985d7a5554e2fd86c0e5c4e4c2d928
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-08-23 21:27:01 +0800

    Update Result type, add Ok and Err types

--- src/types/result.types.ts
+++ src/types/result.types.ts
@@ -1,11 +1,27 @@
+/** The successful {@linkcode Result} type. */
+export type Ok<T> = [null, T] & {
+  /** A boolean that determines if the result resolved properly or not. */
+  ok: true;
+  /** The original value. */
+  value: T;
+  /** The caught error. */
+  error: null;
+};
+
+/** The error {@linkcode Result} type. */
+export type Err<T> = [T, null] & {
+  /** A boolean that determines if the result resolved properly or not. */
+  ok: false;
+  /** The original value. */
+  value: null;
+  /** The caught error. */
+  error: T;
+};
+
 /**
- * The Result type. Values change based on the result.
- * ```
- * // success
- * { ok: true, value: T, error: null }
- * // fail
- * { ok: false, value: null, error: E }
- * ```
+ * The Result type. Value changes based on the result.
+ * - {@linkcode Ok<T>} - The successful result.
+ * - {@linkcode Err<E>} - The error result.
  *
  * Can be used like an array or object.
  * ```
@@ -22,20 +38,4 @@
  * @template T The value type.
  * @template E The error type.
  */
-export type Result<T, E = unknown> =
-  | ([null, T] & {
-      /** A boolean that determines if the result resolved properly or not. */
-      ok: true;
-      /** The original value. */
-      value: T;
-      /** The caught error. */
-      error: null;
-    })
-  | ([E, null] & {
-      /** A boolean that determines if the result resolved properly or not. */
-      ok: false;
-      /** The original value. */
-      value: null;
-      /** The caught error. */
-      error: E;
-    });
+export type Result<T, E = unknown> = Ok<T> | Err<E>;
